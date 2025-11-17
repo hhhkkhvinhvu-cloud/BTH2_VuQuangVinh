ccf378e7add2224ee6a813adf7f5a3941939208d
lahu <lanh2211pc@gmail.com>
2025-11-17 11:44:29 +0700

    feat: Demo taikhoan API

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { getAllUsers, getUserById } from "../controllers/userController.js";
-
+import {
+  getAllAccounts,
+  getAccountByTenTK,
+} from "../controllers/taikhoanController.js";
 
 const router = express.Router();
 
@@ -8,11 +11,16 @@ router.get("/", (req, res) => {
   res.json({ message: "Welcome to API route" });
 });
 
+// -----------------------users---------------------------
 // Route: GET /api/users
 router.get("/users/", getAllUsers);
-
 // Route: GET /api/users/:id
-router.get("/users/:id", getUserById)
+router.get("/users/:id", getUserById);
 
+// -----------------------Tai Khoan---------------------------
+// Route: GET /api/accounts
+router.get("/accounts/", getAllAccounts);
+// Route: GET /api/accounts/:tentk
+router.get("/accounts/:tentk", getAccountByTenTK);
 
 export default router;
